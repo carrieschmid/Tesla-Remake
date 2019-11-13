bb5f7a4b4948e815521dfc54f8ab821eca116e0d
carriepederson7@gmail.com and haileygaylor@gmail.com <example@example.com>
2019-11-13 14:51:21 -0800

    completed header nav bar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,6 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div> 
-      <h1>Tesla</h1>
       <Header/>
       <Intro/>
       <SeeAllJobs/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  var headerStyle ={
+    backgroundColor:'#adafb3',
+    paddingLeft: '100px',
+    paddingRight: '100px'
+    
+  };
+  var navBarStyle = {
+    backgroundColor: 'pink',
+    content: 'center',
+    paddingLeft: '30px',
+    paddingRight: '30px'
+  };
+
+  var header ={
+    fontFamily: "TESLA",
+    color: 'white',
+    paddingRight: '30px'
+  }
+
+
+
+
   return (
-    <div>
-      <Link to="/">Model S</Link>
-      <Link to="/">Model 3</Link>
-      <Link to="/">Model X</Link>
-      <Link to="/">Model Y</Link>
-      <Link to="/">Roadster</Link>
-      <Link to="/">Energy</Link>
+    <div className="navbar" style = {headerStyle}>
+
+
+
+    <h3 style={header} className= "navbar-brand">Tesla</h3>
+      <navbar/>
+        <navLink to="/">Model S</navLink>
+        <navLink to="/">Model 3</navLink>
+        <navLink to="/">Model X</navLink>
+        <navLink to="/">Model Y</navLink>
+        <navLink to="/">Roadster</navLink>
+        <navLink to="/">Energy</navLink>
     </div>
 
   );
